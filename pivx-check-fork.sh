de273727daef88fb87195dd3e5dc45f8d5ee170c
go140point6 <50885069+go140point6@users.noreply.github.com>
2019-05-30 10:50:22 -0700

    Changed explorer, slowed api calls

--- pivx-check-fork.sh
+++ pivx-check-fork.sh
@@ -8,31 +8,63 @@ set -e
 
 pivxcli=~/pivx/bin/pivx-cli
 
-function getBlockNumber() {
+function getLocalBlockNumber() {
   $pivxcli getinfo | grep blocks | sed "s/.*: \([0-9]\+\).*/\1/"
+}
+
+function getRemoteBlockNumber() {
+  curl -s 'https://pivx.ccore.online/api/getblockcount'
+  # need to slow calls to ccore api
+  sleep 1
 }
 
 function getLocalBlockHash() {
   $pivxcli getblockhash $1
+  # extra sleep to slow calls to ccore api
+  sleep 1
 }
 
 function getRemoteBlockHash() {
-#  curl -# 'https://pivx.ccore.online/api/getblockhash?index='$1
-   curl -# 'https://chainz.cryptoid.info/pivx/api.dws?q=getblockhash&height='$1 | tr -d '"'
+   curl -s 'https://pivx.ccore.online/api/getblockhash?index='$1
 }
 
-blockNumber=`getBlockNumber`
-localBlockHash=`getLocalBlockHash $blockNumber`
-remoteBlockHash=`getRemoteBlockHash $blockNumber`
+localBlockNumber=`getLocalBlockNumber`
+remoteBlockNumber=`getRemoteBlockNumber`
+localBlockHash=`getLocalBlockHash $localBlockNumber`
+remoteBlockHash=`getRemoteBlockHash $remoteBlockNumber`
 
-echo '            block: '$blockNumber
-echo ' block hash local: '$localBlockHash
-echo 'block hash remote: '$remoteBlockHash
+#echo '        local block: '$localBlockNumber
+#echo '       remote block: '$remoteBlockNumber
+#echo '   block hash local: '$localBlockHash
+#echo '  block hash remote: '$remoteBlockHash
+
+# "root" corresponds to /etc/aliases (place email address there) and run newaliases
+# or could also just put your email address here (no quotes)
+if [[ "$remoteBlockHash" == *"data error"* ]]; then
+  mail -s "[pivx] slowpoke alert" "root" <<EOF
+  The PIVX blockchain explorer at pivx.ccore.online is behind the current block count
+
+   Local block:  $localBlockNumber
+   Remote block: $remoteBlockNumber
+EOF
+exit 2
+fi
 
 if [ $localBlockHash == $remoteBlockHash ]; then
-  echo 'GOOD: still on main'
+  mail -s "[pivx] pivx OK!" "root" <<EOF
+  GOOD: current pivx block is $localBlockNumber and still on main
+
+   Local Hash:   $localBlockHash
+  Remote Hash:  $remoteBlockHash
+EOF
 else
-  echo 'BAD: you are on a fork, got to re-sync'
+  mail -s "[pivx] pivx may be forked!" "root" <<EOF
+  BAD: current local pivx block is $localBlockNumber but possible hash mismatch:
+
+ Remote Block:  $remoteBlockNumber
+   Local Hash:     $localBlockHash
+  Remote Hash:    $remoteBlockHash
+EOF
 fi
 
 exit 0
