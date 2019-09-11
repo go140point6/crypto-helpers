b70a42aed135f05153eb18002f81041f32472a31
go140point6 <50885069+go140point6@users.noreply.github.com>
2019-09-11 16:17:59 -0700

    Check for mature coins, no email if found

--- navcoin-monitor-staking.sh
+++ navcoin-monitor-staking.sh
@@ -15,15 +15,26 @@ function getStakingInfo() {
 
 enabled="$($cli getstakinginfo | jq -r '.enabled')"
 staking="$($cli getstakinginfo | jq -r '.staking')"
+errors="$($cli getstakinginfo | jq -r '.errors')"
 
-echo $dt "e:$enabled" "s:$staking" >> ~/navcoin/bin/results.log
+# check if $errors is null and replace with text if so
+if [ -z "$errors" ]; then
+  errors="none"
+fi
 
-if [[ $enabled != "true" || $staking != "true" ]]; then
+echo $dt "enabled:$enabled" "staking:$staking" "errors:$errors" >> ~/navcoin/bin/results.log
+
+# "root" corresponds to /etc/aliases (place email address there) and run newaliases
+# or could also just put your email address here (no quotes)
+if [[ $staking == "true" || $errors="Warning: We don't appear to have mature coins." ]]; then
+  # do nothing, staking is OK or we just got a deposit
+  exit
+else
   mail -s "[nav] stake heartbeat" "root" <<EOF
 
   Hourly heartbeat report $dt
 
-  Navcoin wallet is locked or otherwise not staking
+  Navcoin wallet is locked or otherwise not staking for some reason
 
   `getStakingInfo`
 EOF
